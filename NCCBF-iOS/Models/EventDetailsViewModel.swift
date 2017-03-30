b0f123e75e24a2e9a80359cc17fc3458503795b8
Keita Ito <keitaitok@gmail.com>
2017-03-29 17:06:23 -0700

    Update EventDetailsViewModel endAt property of type Optional String

--- NCCBF-iOS/Models/EventDetailsViewModel.swift
+++ NCCBF-iOS/Models/EventDetailsViewModel.swift
@@ -24,15 +24,16 @@ struct EventDetailsViewModel {
         return dateFormatter.string(from: event.startAt)
     }
     
-    public var endAt: String {
-        return dateFormatter.string(from: event.endAt)
+    public var endAt: String? {
+        guard let endAt = event.endAt else { return nil }
+        return dateFormatter.string(from: endAt)
     }
     
     public var schedule: String {
         let df = DateFormatter()
         df.dateFormat = "h:mm a"
         df.timeZone = TimeZone(abbreviation: "PST")
-        let endAtString = df.string(from: event.endAt)
+        let endAtString = endAt ?? "all day"
         return "\(startAt) - \(endAtString)"
     }
     
