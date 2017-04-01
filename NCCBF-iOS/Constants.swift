6e1f1c34a8b2e41c6fb93dc329fefe3ff5d1353c
Keita Ito <keitaitok@gmail.com>
2017-03-31 19:14:39 -0700

    Add enum ImageHandlingMessage

--- NCCBF-iOS/Constants.swift
+++ NCCBF-iOS/Constants.swift
@@ -14,3 +14,13 @@ let NCCBF2017EventImageURL = URL(string: "https://sfcherryblossom.org/wp-content
 let launchedBefore = "launchedBefore"
 
 let NCCBF2017EventImagesString = "NCCBF2017EventImages"
+
+enum ImageHandlingMessage: String {
+    case foundInAppBundle = "Image is found in the app bundle. It will be displayed."
+    case notFoundInAppBundle = "Image is not found in the app bundle. Check caches directory."
+    case foundInCachesDirectory = "Image is found in caches directory. It will be displayed."
+    case notFoundInCachesDirectory = "Image is not found in caches directory. Download image from the server."
+    case saveDownloadedImageInCachesDirectory = "Image is donwloaded from the server. it is not found in caches directory. Save it."
+    case writingImageSucceeded = "Writing the image succeeded."
+}
+
