d8aefd371ac8ad1835a8562e5fb42ed427c3e860
Keita Ito <keitaitok@gmail.com>
2017-04-01 18:01:08 -0700

    Add UIColorExtension.swift
    - Add sakuraPink class computed property

---
+++ NCCBF-iOS/Extensions/UIColorExtension.swift
@@ -0,0 +1,16 @@
+//
+//  UIColorExtension.swift
+//  NCCBF-iOS
+//
+//  Created by Keita Ito on 4/1/17.
+//  Copyright © 2017 Keita Ito. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    class var sakuraPink: UIColor {
+        return UIColor(red: 0.957, green: 0.549, blue: 0.62, alpha: 1.0)
+//        colorWithRed:0.957 green:0.549 blue:0.62 alpha:1.0]
+    }
+}
