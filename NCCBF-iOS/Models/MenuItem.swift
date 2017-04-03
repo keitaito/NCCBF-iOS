33baf583cf23afad6f3797cff9d431e923ed6e1c
Keita Ito <keitaitok@gmail.com>
2017-04-03 15:55:50 -0700

    Update MoreInfoMenuItem.plist
    - Add Licenses
    - Replace infoLabel with textView

--- NCCBF-iOS/Models/MenuItem.swift
+++ NCCBF-iOS/Models/MenuItem.swift
@@ -27,12 +27,14 @@ extension MenuItem {
         
         let aboutDescription = dictionary["About"] as! String
         let copyrightDescription = dictionary["Copyright"] as! String
+        let licensesDescription = dictionary["Licenses"] as! String
         
         let facebook = MenuItem(title: "Facebook", url: URL(string: "https://www.facebook.com/NCCBF"), info: nil)
         let twitter = MenuItem(title: "Twitter", url: URL(string: "https://twitter.com/NC_CBF"), info: nil)
         let instagram = MenuItem(title: "Instagram", url: URL(string: "https://instagram.com/NC_CBF"), info: nil)
         let aboutPage = MenuItem(title: "About", url: nil, info: aboutDescription)
         let copyRightPage = MenuItem(title: "Copyright", url: nil, info: copyrightDescription)
-        return [facebook, twitter, instagram, aboutPage, copyRightPage]
+        let licensesPage = MenuItem(title: "Licenses", url: nil, info: licensesDescription)
+        return [facebook, twitter, instagram, aboutPage, copyRightPage, licensesPage]
     }
 }
