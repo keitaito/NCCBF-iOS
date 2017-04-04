abb222084ed4c77cfcc60bef4f13e41300852a47
Keita Ito <keitaitok@gmail.com>
2017-04-03 23:41:48 -0700

    Update UI test for fastlane snapshot

--- NCCBF-iOSUITests/NCCBF_iOSUITests.swift
+++ NCCBF-iOSUITests/NCCBF_iOSUITests.swift
@@ -13,14 +13,9 @@ class NCCBF_iOSUITests: XCTestCase {
     override func setUp() {
         super.setUp()
         
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        let app = XCUIApplication()
+        setupSnapshot(app)
+        app.launch()
     }
     
     override func tearDown() {
@@ -31,6 +26,27 @@ class NCCBF_iOSUITests: XCTestCase {
     func testExample() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        let app = XCUIApplication()
+        sleep(3)
+        let tabBarsQuery = app.tabBars
+        tabBarsQuery.buttons["Events"].tap()
+        snapshot("01EventCatalogScene")
+        
+        let tablesQuery = app.tables
+        tablesQuery.children(matching: .cell).element(boundBy: 0).staticTexts["San Francisco Taiko Dojo"].tap()
+        snapshot("02EventDetailsScene")
+        
+        app.navigationBars["San Francisco Taiko Dojo"].buttons["Events"].tap()
+        
+        tabBarsQuery.buttons["Schedule"].tap()
+        snapshot("03ScheduleScene")
+        
+        tabBarsQuery.buttons["Map"].tap()
+        app.otherElements["Peace Plaza Stage"].tap()
+        snapshot("04MapScene")
+        
+        tabBarsQuery.buttons["More"].tap()
     }
     
 }
