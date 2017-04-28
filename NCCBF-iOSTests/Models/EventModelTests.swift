6fdd51219b3b6ad7dc35932afb37ed8ff58f621c
Keita Ito <keitaitok@gmail.com>
2017-04-28 16:57:55 -0700

    Add EventModelTests.swift

---
+++ NCCBF-iOSTests/Models/EventModelTests.swift
@@ -0,0 +1,12 @@
+//
+//  EventModelTests.swift
+//  NCCBF-iOS
+//
+//  Created by Keita Ito on 4/28/17.
+//  Copyright © 2017 Keita Ito. All rights reserved.
+//
+
+import XCTest
+
+class EventModelTests: XCTestCase {
+}
