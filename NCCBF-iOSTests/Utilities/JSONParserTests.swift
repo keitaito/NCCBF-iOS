b6da2827a84d6a33a2c684878daa303d40a30a17
Keita Ito <keitaitok@gmail.com>
2017-04-29 19:23:11 -0700

    Add JSONParserTests.swift

---
+++ NCCBF-iOSTests/Utilities/JSONParserTests.swift
@@ -0,0 +1,12 @@
+//
+//  JSONParserTests.swift
+//  NCCBF-iOS
+//
+//  Created by Keita Ito on 4/29/17.
+//  Copyright © 2017 Keita Ito. All rights reserved.
+//
+
+import XCTest
+
+class JSONParserTests: XCTestCase {    
+}
