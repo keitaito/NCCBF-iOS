cf74e35d6d68d47694c6ea308e3e2fbc4c2af1a7
Keita Ito <keitaitok@gmail.com>
2017-04-29 20:06:09 -0700

    Add TestHelper class

--- NCCBF-iOSTests/TestHelpers/TestHelper.swift
+++ NCCBF-iOSTests/TestHelpers/TestHelper.swift
@@ -7,3 +7,15 @@
 //
 
 import Foundation
+
+class TestHelper {
+    class func jsonFromBundle() -> Any {
+        let filePath = Bundle.main.path(forResource: "NCCBFSchedule", ofType: "json")!
+        do {
+            let jsonData = try Data(contentsOf: URL(fileURLWithPath: filePath))
+            return try JSONSerialization.jsonObject(with: jsonData)
+        } catch {
+            fatalError(error.localizedDescription)
+        }
+    }
+}
