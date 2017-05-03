0fac1762425e13ed07c4892a092ffe40ce332086
Keita Ito <keitaitok@gmail.com>
2017-05-02 22:00:46 -0700

    Add EventLocationTests.swift

---
+++ NCCBF-iOSTests/Models/EventLocationTests.swift
@@ -0,0 +1,12 @@
+//
+//  EventLocationTests.swift
+//  NCCBF-iOS
+//
+//  Created by Keita Ito on 5/2/17.
+//  Copyright © 2017 Keita Ito. All rights reserved.
+//
+
+import XCTest
+
+class EventLocationTests: XCTestCase {    
+}
